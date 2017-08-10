516b010cb31c8a26ee9c4a1f6faf728dfbf909d5
Eleanor Lee <ellie007@gmail.com>
2017-08-10 16:08:22 -0500

    user sessions route, controller, view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  root 'sessions#new'
+
+  get '/sessions/new' => 'sessions#new', :as => 'new_session'
+  post '/sessions' => 'sessions#create', :as => 'sessions'
+  delete '/sessions' => 'sessions#destroy', :as => 'session'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
